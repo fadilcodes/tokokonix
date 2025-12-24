782b355c4b9edc423995bd6f0e52266920517b45
samaloop <loop.samaloop@gmail.com>
2025-12-24 22:02:01 +0700

    Force HTTPS schema for assets lagi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- JANGAN LUPA TAMBAH INI DI ATAS
 
 class AppServiceProvider extends ServiceProvider
 {
